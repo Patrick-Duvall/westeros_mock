0ce4ac200038b0d1ac3529e827bded33873b788f
Patrick Duvall <duvallp208@gmail.com>
2019-07-19 11:14:02 -0600

    change dropdown put header in conn

--- app/services/westeros_service.rb
+++ app/services/westeros_service.rb
@@ -1,19 +1,24 @@
 class WesterosService
 
   def initialize(house_name)
-    @house_name = house_name
+    @house_name = house_name.
   end
 
   def members
-    response = conn.get("/api/v1/house/#{@house_name}?api_key=#{ENV['WESTEROS_KEY']}")
+    require "pry"; binding.pry
+
+    JSON.parse(conn.get("/api/v1/house/").body)
+    response = conn.get("/api/v1/house/#{@house_name}")
     JSON.parse(response.body)['data'][0]['attributes']['members']
   end
 
   private
 
   def conn
-    conn = Faraday.new(:url => "http://westerosapi.herokuapp.com") do |faraday|
-      faraday.adapter  Faraday.default_adapter  
+    conn = Faraday.new(:url => "http://westeros-as-a-service.herokuapp.com") do |faraday|
+      faraday.headers['x_api_key'] = ENV['WESTEROS_KEY']
+      faraday.headers['Content-Type'] = 'application/json'
+      faraday.adapter  Faraday.default_adapter
     end
 
   end
